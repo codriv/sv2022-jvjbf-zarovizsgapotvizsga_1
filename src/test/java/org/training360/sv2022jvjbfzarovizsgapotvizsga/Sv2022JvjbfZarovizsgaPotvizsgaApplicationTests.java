b5c98fd52b510af6eec960fc4cc6f3ffb9eb3d27
codriv <combodriver4@yahoo.at>
2022-07-18 21:35:58 +0200

    init

---
+++ src/test/java/org/training360/sv2022jvjbfzarovizsgapotvizsga/Sv2022JvjbfZarovizsgaPotvizsgaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training360.sv2022jvjbfzarovizsgapotvizsga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sv2022JvjbfZarovizsgaPotvizsgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
